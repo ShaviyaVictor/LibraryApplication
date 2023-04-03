94e7020ab944044adf1db542bf7c05495b8d123d
Victor Shaviya <vs610642@digicelgroup.com>
2023-04-03 15:23:17 -0500

    Add the @Service annotation and call the latency simulation within the method execution

--- src/main/java/com/shavic/LibraryApplication/service/BookRepositoryImpl.java
+++ src/main/java/com/shavic/LibraryApplication/service/BookRepositoryImpl.java
@@ -2,16 +2,20 @@ package com.shavic.LibraryApplication.service;
 
 import com.shavic.LibraryApplication.model.Book;
 import com.shavic.LibraryApplication.repository.BookRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookRepositoryImpl implements BookRepository {
 
     @Override
     public Book getByIsbn(String isbn) {
+//        deliberately insert a 3-sec delay into each method call
+        simulateSlowService();
         return new Book(isbn,"Sample Book");
     }
 
 //    simulating some latency(network service or just some issue & in our case slow delay)
-//    and calling it in the method implementation
+//    and calling it in the method implementation for a 3-sec delay
     private void simulateSlowService() {
         try {
             long time = 3000L;
