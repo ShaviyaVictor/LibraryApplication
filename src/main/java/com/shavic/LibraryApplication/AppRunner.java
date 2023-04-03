ff23ef1cb73a5fe9a14a83fdf46cacfd6cd1a04e
Victor Shaviya <vs610642@digicelgroup.com>
2023-04-03 16:18:11 -0500

    Create the AppRunner class that implements the CommandLineRunner which injects the BookRepository

---
+++ src/main/java/com/shavic/LibraryApplication/AppRunner.java
@@ -0,0 +1,26 @@
+package com.shavic.LibraryApplication;
+
+import com.shavic.LibraryApplication.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+//create the AppRunner class which implements the CommandLineRunner
+// that injects the BookRepository and calls it different several times with different arguments
+@Component
+public class AppRunner implements CommandLineRunner {
+
+    public static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
+    private final BookRepository bookRepository;
+
+
+    public AppRunner(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
