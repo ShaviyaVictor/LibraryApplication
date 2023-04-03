90c1c9383e56879d22c1173fdcbaf23edb049ffe
Victor Shaviya <vs610642@digicelgroup.com>
2023-04-03 13:10:35 -0500

    Create the constructors getters and setters

--- src/main/java/com/shavic/LibraryApplication/model/Book.java
+++ src/main/java/com/shavic/LibraryApplication/model/Book.java
@@ -5,10 +5,25 @@ public class Book {
     private String isbn;
     private String title;
 
+//    create the model's constructors
     public Book(String isbn, String title) {
         this.isbn = isbn;
         this.title = title;
     }
 
+//    create the constructor's getters and setters
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
 }
