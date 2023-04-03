4b2e690f72ed5ff1ee5737f92409f6da4defca20
Victor Shaviya <vs610642@digicelgroup.com>
2023-04-03 13:03:30 -0500

    Create the Book model and add the constructor for the model

---
+++ src/main/java/com/shavic/LibraryApplication/model/Book.java
@@ -0,0 +1,14 @@
+package com.shavic.LibraryApplication.model;
+
+public class Book {
+
+    private String isbn;
+    private String title;
+
+    public Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+
+}
