d8d064bf14731cb287afffdb51f139ef325354eb
Victor Shaviya <vs610642@digicelgroup.com>
2023-04-04 14:51:58 -0500

    Enable caching for the getBy.. method using @Cachable annotation

--- src/main/java/com/shavic/LibraryApplication/service/BookRepositoryImpl.java
+++ src/main/java/com/shavic/LibraryApplication/service/BookRepositoryImpl.java
@@ -2,12 +2,14 @@ package com.shavic.LibraryApplication.service;
 
 import com.shavic.LibraryApplication.model.Book;
 import com.shavic.LibraryApplication.repository.BookRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookRepositoryImpl implements BookRepository {
 
     @Override
+    @Cacheable("books")
     public Book getByIsbn(String isbn) {
 //        deliberately insert a 3-sec delay into each method call
         simulateSlowService();
