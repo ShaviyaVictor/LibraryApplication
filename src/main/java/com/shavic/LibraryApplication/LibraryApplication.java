eb22c30ab23ec7ac96453326896cb9e3ed6988c8
Victor Shaviya <vs610642@digicelgroup.com>
2023-04-04 15:16:33 -0500

    Enabling the processing of the caching annotations

--- src/main/java/com/shavic/LibraryApplication/LibraryApplication.java
+++ src/main/java/com/shavic/LibraryApplication/LibraryApplication.java
@@ -2,8 +2,11 @@ package com.shavic.LibraryApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+// Enabling the processing of the caching annotations
 @SpringBootApplication
+@EnableCaching
 public class LibraryApplication {
 
 	public static void main(String[] args) {
