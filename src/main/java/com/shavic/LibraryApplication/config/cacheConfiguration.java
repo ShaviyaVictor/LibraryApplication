fca6fd0dc1bb152a36417f7385b848d7366cce5d
ShaviyaVictor <shaviyavictor@gmail.com>
2023-04-05 13:00:34 -0500

    Add the Customizer interface for Concurrent caching and inplement the interface method

--- src/main/java/com/shavic/LibraryApplication/config/cacheConfiguration.java
+++ src/main/java/com/shavic/LibraryApplication/config/cacheConfiguration.java
@@ -1,10 +1,22 @@
 package com.shavic.LibraryApplication.config;
 
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class cacheConfiguration {
 
+    CacheManagerCustomizer<ConcurrentMapCacheManager> customizer() {
+        return new ConcurrentCustomizer();
+    }
 
+    class ConcurrentCustomizer implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
+
+        @Override
+        public void customize(ConcurrentMapCacheManager cacheManager) {
+
+        }
+    }
 
 }
