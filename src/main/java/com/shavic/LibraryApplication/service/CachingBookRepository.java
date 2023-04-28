750d08a33132647aa5c715c9aad25002ac69d00d
ShaviyaVictor <shaviyavictor@gmail.com>
2023-04-28 17:07:27 -0500

    Create a new Repository implementation class for caching

---
+++ src/main/java/com/shavic/LibraryApplication/service/CachingBookRepository.java
@@ -0,0 +1,13 @@
+package com.shavic.LibraryApplication.service;
+
+import com.shavic.LibraryApplication.model.Book;
+import com.shavic.LibraryApplication.repository.BookRepository;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CachingBookRepository {
+
+
+
+}
